e2d5d40cfdd6b33d4a99c53b000a4683175b59a0
Mark Philipp <mphilipp622@gmail.com>
2023-09-20 12:46:15 -0700

    Added Table

---
+++ src/OJTable.js
@@ -0,0 +1,72 @@
+﻿import React, {useEffect, useState} from 'react';
+import {ParsedOJ} from "./ParsedOJ";
+import Box from '@mui/material/Box';
+import {DataGrid} from '@mui/x-data-grid';
+import {CircularProgress, Paper} from "@mui/material";
+import {OJ_DICT} from "./OrthogonalJones";
+
+const columns = [
+    {field: 'originalString', headerName: 'Name', width: 400},
+    {field: 'numDirectionChanges', headerName: 'Dir Changes', type: 'number', width: 150},
+    {field: 'numFrames', headerName: 'Frames', type: 'number', width: 150},
+    {field: 'height', headerName: 'Height', type: 'number', width: 150},
+    {field: 'numUp', headerName: 'Up', type: 'number', width: 150},
+    {field: 'numDown', headerName: 'Down', type: 'number', width: 150},
+    {field: 'numLeft', headerName: 'Left', type: 'number', width: 150},
+    {field: 'numRight', headerName: 'Right', type: 'number', width: 150},
+    {field: 'numNeutral', headerName: 'Neutral', type: 'number', width: 150},
+    {field: 'numBuffer', headerName: 'Buffer', type: 'number', width: 150},
+];
+
+function OJTable() {
+    const [parsedData, setParsedData] = useState([]);
+    const [tableData, setTableData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        setIsLoading(true);
+
+        let newTableData = []
+        let newParsedData = []
+        for (let i = 0; i < OJ_DICT.length; i++) {
+            let inputString = OJ_DICT[i];
+            let parsedOj = new ParsedOJ(inputString);
+            newParsedData.push(parsedOj);
+            newTableData.push(parsedOj.GetTableData());
+        }
+
+        setTableData(newTableData);
+        setParsedData(newParsedData);
+        setIsLoading(false);
+
+        // You can perform actions like data fetching or setting up event listeners here
+    }, []); // The empty array [] as the second argument ensures it only runs once on mount
+
+    if (isLoading) return <CircularProgress/>
+
+    return (
+        <Box sx={{width: '100%', height: "100%"}}>
+            <Paper sx={{width: '100%', height: "100%", mb: 2}}>
+                <DataGrid
+                    rows={tableData}
+                    columns={columns}
+                    slots={{
+                        toolbar: () => <Box>
+                            <h4>Orthogonal Jones Table</h4>
+                            <text><b><i>Hover cell headers for filtering options</i></b></text>
+                        </Box>
+                    }}
+                    initialState={{
+                        pagination: {
+                            paginationModel: {page: 0, pageSize: 50},
+                        },
+                    }}
+                    pageSizeOptions={[5, 10, 50, 100, 1000]}
+                    showCellVerticalBorder={true}
+                />
+            </Paper>
+        </Box>
+    );
+}
+
+export default OJTable;
---
+++ src/ParsedOJ.js
@@ -0,0 +1,83 @@
+﻿const pattern = /([BULRDN]*)[_]*(\d)[_](\d{4})/;
+
+export class ParsedOJ {
+    
+    constructor(originalString) {
+        this.originalString = originalString;
+        this.numUp = 0;
+        this.numDown = 0;
+        this.numLeft = 0;
+        this.numRight = 0;
+        this.numBuffer = 0;
+        this.numFrames = 0;
+        this.numNeutral = 0;
+        this.numDirectionChanges = 0;
+        this.height = 0;
+        
+        const match = originalString.match(pattern);
+        if (match) {
+            const letters = match[1];
+            this.numFrames = match[2];
+            this.height = match[3];
+
+            // Parse letters
+            const letterCounts = {
+                'B': 0,
+                'N': 0,
+                'U': 0,
+                'D': 0,
+                'L': 0,
+                'R': 0
+            };
+
+            let previousLetter = '~';
+            for (const letter of letters) {
+                letterCounts[letter] += 1;
+
+                if (previousLetter !== '~' && previousLetter !== letter) {
+                    this.numDirectionChanges += 1;
+                }
+
+                previousLetter = letter;
+            }
+
+            this.numUp = letterCounts['U'];
+            this.numDown = letterCounts['D'];
+            this.numLeft = letterCounts['L'];
+            this.numRight = letterCounts['R'];
+            this.numBuffer = letterCounts['B'];
+            this.numNeutral = letterCounts['N'];
+        } else {
+            throw new Error(`Unable to parse ${originalString}`);
+        }
+    }
+
+    Print() {
+        console.log(`Original String = ${this.originalString}`);
+        console.log(`# Up = ${this.numUp}`);
+        console.log(`# Down = ${this.numDown}`);
+        console.log(`# Left = ${this.numLeft}`);
+        console.log(`# Right = ${this.numRight}`);
+        console.log(`# Neutral = ${this.numNeutral}`);
+        console.log(`# Buffers = ${this.numBuffer}`);
+        console.log(`# Direction Changes = ${this.numDirectionChanges}`);
+        console.log(`frames = ${this.numFrames}`);
+        console.log(`height = ${this.height}`);
+    }
+    
+    GetTableData(){
+        return{
+            id: this.originalString,
+            originalString: this.originalString,
+            numDirectionChanges: this.numDirectionChanges,
+            numFrames: this.numFrames,
+            height: this.height,
+            numUp: this.numUp,
+            numDown: this.numDown,
+            numLeft: this.numLeft,
+            numRight: this.numRight,
+            numNeutral: this.numNeutral,
+            numBuffer: this.numBuffer
+        }
+    }
+}
