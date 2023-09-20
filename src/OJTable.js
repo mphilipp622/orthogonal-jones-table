2c889339ed45b7d592eda9a4ef969e9fd3a45dc6
Mark Philipp <mphilipp622@gmail.com>
2023-09-20 13:28:49 -0700

    Added Z Values

--- src/OJTable.js
+++ src/OJTable.js
@@ -3,19 +3,24 @@ import {ParsedOJ} from "./ParsedOJ";
 import Box from '@mui/material/Box';
 import {DataGrid} from '@mui/x-data-grid';
 import {CircularProgress, Paper} from "@mui/material";
-import {OJ_DICT} from "./OrthogonalJones";
+import {OJ_DICT_ZED} from "./OrthogonalJones";
 
 const columns = [
     {field: 'originalString', headerName: 'Name', width: 400},
+    {field: 'frame', headerName: 'Frame', type: 'number', width: 150},
     {field: 'numDirectionChanges', headerName: 'Dir Changes', type: 'number', width: 150},
-    {field: 'numFrames', headerName: 'Frames', type: 'number', width: 150},
     {field: 'height', headerName: 'Height', type: 'number', width: 150},
+    {field: 'zed0', headerName: 'Zed 1', type: 'number', width: 150},
+    {field: 'zed1', headerName: 'Zed 2', type: 'number', width: 150},
+    {field: 'zed2', headerName: 'Zed 3', type: 'number', width: 150},
+    {field: 'zed3', headerName: 'Zed 4', type: 'number', width: 150},
+    {field: 'zed4', headerName: 'Zed 5', type: 'number', width: 150},
     {field: 'numUp', headerName: 'Total Up', type: 'number', width: 150},
     {field: 'numDown', headerName: 'Total Down', type: 'number', width: 150},
     {field: 'numLeft', headerName: 'Total Left', type: 'number', width: 150},
     {field: 'numRight', headerName: 'Total Right', type: 'number', width: 150},
     {field: 'numNeutral', headerName: 'Total Neutral', type: 'number', width: 150},
-    {field: 'numBuffer', headerName: 'Total Buffer', type: 'number', width: 150},
+    {field: 'numBonk', headerName: 'Total Bonks', type: 'number', width: 150},
 ];
 
 function OJTable() {
@@ -28,8 +33,8 @@ function OJTable() {
 
         let newTableData = []
         let newParsedData = []
-        for (let i = 0; i < OJ_DICT.length; i++) {
-            let inputString = OJ_DICT[i];
+        for (let i = 0; i < OJ_DICT_ZED.length; i++) {
+            let inputString = OJ_DICT_ZED[i];
             let parsedOj = new ParsedOJ(inputString);
             newParsedData.push(parsedOj);
             newTableData.push(parsedOj.GetTableData());
@@ -60,6 +65,9 @@ function OJTable() {
                         pagination: {
                             paginationModel: {page: 0, pageSize: 50},
                         },
+                        sorting: {
+                            sortModel: [{field: 'frame', sort: 'asc'}]
+                        }
                     }}
                     pageSizeOptions={[5, 10, 50, 100, 1000]}
                     showCellVerticalBorder={true}
--- src/ParsedOJ.js
+++ src/ParsedOJ.js
@@ -1,4 +1,5 @@
 ﻿const pattern = /([BULRDN]*)[_]*(\d)[_](\d{4})/;
+const patternZed = /([BULRDN]*)[_]*(\d)[_](\d{4})_([+-]?[0-9]*[.]?[0-9]+)_([+-]?[0-9]*[.]?[0-9]+)_([+-]?[0-9]*[.]?[0-9]+)_([+-]?[0-9]*[.]?[0-9]+)_([+-]?[0-9]*[.]?[0-9]+)/;
 
 export class ParsedOJ {
     
@@ -8,17 +9,23 @@ export class ParsedOJ {
         this.numDown = 0;
         this.numLeft = 0;
         this.numRight = 0;
-        this.numBuffer = 0;
-        this.numFrames = 0;
+        this.numBonk = 0;
+        this.frame = 0;
         this.numNeutral = 0;
         this.numDirectionChanges = 0;
         this.height = 0;
+        this.zed = [0.0, 0.0, 0.0, 0.0, 0.0];
         
-        const match = originalString.match(pattern);
+        const match = originalString.match(patternZed);
         if (match) {
             const letters = match[1];
-            this.numFrames = match[2];
+            this.numDirectionChanges = match[2];
             this.height = match[3];
+            this.zed[0] = match[4];
+            this.zed[1] = match[5];
+            this.zed[2] = match[6];
+            this.zed[3] = match[7];
+            this.zed[4] = match[8];
 
             // Parse letters
             const letterCounts = {
@@ -33,19 +40,14 @@ export class ParsedOJ {
             let previousLetter = '~';
             for (const letter of letters) {
                 letterCounts[letter] += 1;
-
-                if (previousLetter !== '~' && previousLetter !== letter) {
-                    this.numDirectionChanges += 1;
-                }
-
-                previousLetter = letter;
             }
 
             this.numUp = letterCounts['U'];
             this.numDown = letterCounts['D'];
             this.numLeft = letterCounts['L'];
             this.numRight = letterCounts['R'];
-            this.numBuffer = letterCounts['B'];
+            this.numBonk = letterCounts['B'];
+            this.frame = this.numBonk + 1;
             this.numNeutral = letterCounts['N'];
         } else {
             throw new Error(`Unable to parse ${originalString}`);
@@ -59,25 +61,31 @@ export class ParsedOJ {
         console.log(`# Left = ${this.numLeft}`);
         console.log(`# Right = ${this.numRight}`);
         console.log(`# Neutral = ${this.numNeutral}`);
-        console.log(`# Buffers = ${this.numBuffer}`);
+        console.log(`# Bonks = ${this.numBonk}`);
         console.log(`# Direction Changes = ${this.numDirectionChanges}`);
         console.log(`frames = ${this.numFrames}`);
         console.log(`height = ${this.height}`);
+        console.log(`zed = ${this.zed}`);
     }
     
     GetTableData(){
         return{
             id: this.originalString,
             originalString: this.originalString,
+            frame: this.frame,
             numDirectionChanges: this.numDirectionChanges,
-            numFrames: this.numFrames,
             height: this.height,
             numUp: this.numUp,
             numDown: this.numDown,
             numLeft: this.numLeft,
             numRight: this.numRight,
             numNeutral: this.numNeutral,
-            numBuffer: this.numBuffer
+            numBonk: this.numBonk,
+            zed0: this.zed[0],
+            zed1: this.zed[1],
+            zed2: this.zed[2],
+            zed3: this.zed[3],
+            zed4: this.zed[4],
         }
     }
 }
